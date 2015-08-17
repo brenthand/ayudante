c0e77a1b0b856af3ca6ec3f0d98ec92154898749
brent hand <hand.brent@quantumwebsystems.com>
2015-08-16 20:01:39 -0700

    added more functions and started main function

--- ayudante.js
+++ ayudante.js
@@ -1,5 +1,5 @@
 /*########################################################################################
-		                        _______                                                         
+		                    _______                                                         
                             \  ___ `'.                _..._                  __.....__      
         .-.          .-      ' |--.\  \             .'     '.            .-''         '.    
          \ \        / /      | |    \  '           .   .-.   .     .|   /     .-''"'-.  `.  
@@ -26,7 +26,7 @@ function get_all_img() {
   	if(! $(this).attr("alt") >= 1) { // At does not exist
 			
     	$( this ).attr('alt', $(this).attr('src'));
-    	$( this ).attr('aria-hidden', true);
+    	//$( this ).attr('aria-hidden', true);
 		}	 
 });
 }
@@ -79,23 +79,124 @@ function has_onclick_remove(e) {
 }
 
 
+/*
+	find first h1 and add attribute main-content="true"
+
+*/
+function find_main_content() {
+	//set attribute on first h1 for main content
+	// !!!!!!!!!!!!!!!!!!!! need to add error handling on code below
+	var x = document.getElementsByTagName('h1')[0].setAttribute("id", "ayu-main-content");
+	
+	//add skip to main content link
+	var skip = document.createElement('div');
+	skip.id = "ayu-skip-to";
+	var e = document.getElementsByTagName('body')[0];
+	e.insertBefore(skip, e.firstChild);
+	// need to add style or class for style !!!!!!!!!!!!!!!!!!!!!
+	document.getElementById("ayu-skip-to").innerHTML = "<a id='ayu-skip-to-link' href='#ayu-main-content'> Skipt to main content</a>";
+	
+}
+	
+	
+	
+function setCookie(name, v, t) {
+		var d = new Date();
+		d.setTime(d.getTime() + (t*24*60*60*1000));
+		var expire = "expires=" + d.toUTCString();
+		document.cookie = name + "=" + v +"; " + expire;
+	}
+	
+	
+	/*
+		function gets the value of the specified cookie. If cookie does not exist return empty string
+		
+		@params name = the name of the cookie
+	
+	*/
+	function getCookie(name) {
+		name += "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+		}
+		
+		return "";
+	}
+	
+	
+	/*
+		Function to check the values of the cookies used in ayudante
+		
+		********** FOR TEST ONLY ********
+	
+	*/
+	function checkCookie() {
+		
+		var contrast = getCookie("ayudante-contrast");
+		var size = getCookie("ayudante-font");
+		
+		
+		alert("Contrast: " + contrast + "Font: " + size  );
+		
+	}
+	
+	/*
+		Function to delete a cookie by name
+		
+		@params name = the name of the cookie to be deleted
+	
+	*/
+	function delCookie(name) {
+		document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	}
+
+
+function add_style_for_skip_link() {
+	var css = '#ayu-skip-to-link {position:absolute;left:-1000px;top:-1000px;width:1px;height:1px;text-align: left;overflow:hidden;} #ayu-skip-to-link:focus, #ayu-skip-to-link:active, #ayu-skip-to-link:hover {position:absolute;left:0;top:0;width:auto;height:auto;overflow:visible;background-color:#FF3;border: 1px dotted #000;} ',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+	style.type = 'text/css';
+	if (style.styleSheet){
+	  style.styleSheet.cssText = css;
+	} else {
+	  style.appendChild(document.createTextNode(css));
+	}
+
+	head.appendChild(style);
+
+}
+
+
+function replace_br() {
+	$('br').replaceWith(function(){
+    	return $("<p />").append($(this).contents()).css("clear", "both");
+    	
+	});
+
+}
+
 
 
 //Test function when moved to production this function will run all operations on DOM
 
 $(document).ready(function () {
+ 
+add_style_for_skip_link();
+find_main_content();
   
-  var e = document.getElementById("btn");
+get_all_img();
   
-  get_all_img();
-  
-  has_onclick_remove(e); 
-  
-  
-	/*var t0 = performance.now();
-	get_by_attr("alt");
-	var t1 = performance.now();
-	console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")*/
+var x = get_all();
+for (var i = 0; i < x.length; i++) {
+	has_onclick_remove(x[i]);
+}  
+
+replace_br();
+
   
   
 });
